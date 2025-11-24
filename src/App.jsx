ac106c10facba3ae71e830e1fbc7e6cd9f8e55f7
Sakhi <78950775+Sakhi97@users.noreply.github.com>
2025-11-24 19:29:07 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,46 @@ function App() {
       <div className="content">
         <img src={logo} alt="Logo" className="logo" />
 
-        <a href="https://qr.scanned.page/uploads/pdf/Z8ZbrD_9f089aceca1ef8b9.pdf"
-           target="_blank"
-           className="link">Menu</a>
+        <a
+          href="https://qr.scanned.page/uploads/pdf/Z8ZbrD_9f089aceca1ef8b9.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >
+          Menu
+        </a>
 
-        <a href="https://maps.app.goo.gl/64Zpm7KW4A1AQiHF8?g_st=ipc"
-           target="_blank"
-           className="link">Google Maps</a>
+        <a
+          href="https://maps.app.goo.gl/64Zpm7KW4A1AQiHF8?g_st=ipc"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >
+          Google Maps
+        </a>
 
-        <a href="https://www.instagram.com/sushipandadubai?igsh=MWR3cHR0dG5kajhsaw=="
-           target="_blank"
-           className="link">Instagram</a>
+        <a
+          href="https://www.instagram.com/sushipandadubai?igsh=MWR3cHR0dG5kajhsaw=="
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >
+          Instagram
+        </a>
 
-        <a href="https://wa.me/971585837613"
-           target="_blank"
-           className="link">WhatsApp</a>
+        <a
+          href="https://api.whatsapp.com/send?phone=971585837613"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >
+          WhatsApp
+        </a>
       </div>
     </div>
   )
 }
 
 export default App
+
 
