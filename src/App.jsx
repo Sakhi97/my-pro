439eb7946209d1255eca65a2ee26d14afdef1134
Sakhi <78950775+Sakhi97@users.noreply.github.com>
2025-11-24 19:23:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import background from './assets/background.png'
+import logo from './assets/logo.png'
 import './App.css'
 
 function App() {
   return (
-    <div
-      className="app-container"
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <a href="https://qr.scanned.page/uploads/pdf/Z8ZbrD_9f089aceca1ef8b9.pdf" className="link" target="_blank">Menu</a>
-      <a href="https://maps.app.goo.gl/64Zpm7KW4A1AQiHF8?g_st=ipc" className="link" target="_blank">Google maps</a>
-      <a href="https://www.instagram.com/sushipandadubai?igsh=MWR3cHR0dG5kajhsaw==" className="link" target="_blank">Instagram</a>
-      <a href="wa.me/971585837613" className="link" target="_blank">Whats App</a>
+    <div className="app-container">
+      <div className="overlay" />
+
+      <div className="content">
+        <img src={logo} alt="Logo" className="logo" />
+
+        <a href="https://qr.scanned.page/uploads/pdf/Z8ZbrD_9f089aceca1ef8b9.pdf"
+           target="_blank"
+           className="link">Menu</a>
+
+        <a href="https://maps.app.goo.gl/64Zpm7KW4A1AQiHF8?g_st=ipc"
+           target="_blank"
+           className="link">Google Maps</a>
+
+        <a href="https://www.instagram.com/sushipandadubai?igsh=MWR3cHR0dG5kajhsaw=="
+           target="_blank"
+           className="link">Instagram</a>
+
+        <a href="https://wa.me/971585837613"
+           target="_blank"
+           className="link">WhatsApp</a>
+      </div>
     </div>
   )
 }
